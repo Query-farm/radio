28779f842554dfaa0c59b761efa00d2f5df04d6b
Rusty Conover <rusty@conover.me>
2025-05-29 20:19:55 -0400

    fix: wip

--- src/include/radio_subscription.hpp
+++ src/include/radio_subscription.hpp
@@ -36,31 +36,7 @@ private:
 
 public:
 	explicit RadioSubscription(const uint64_t id, const std::string &url, const RadioSubscriptionParameters &params,
-	                           uint64_t creation_time, Radio &radio)
-	    : id_(id), url_(std::move(url)), creation_time_(creation_time), disabled_(false),
-	      received_messages_(params.receive_message_capacity), received_errors_(params.receive_error_capacity),
-	      transmit_messages_(params.transmit_message_capacity, params.transmit_retry_initial_delay_ms,
-	                         params.transmit_retry_multiplier, params.transmit_retry_max_delay_ms),
-	      radio_(radio) {
-		webSocket.setUrl(url_);
-		webSocket.setOnMessageCallback([this](const ix::WebSocketMessagePtr &msg) {
-			if (msg->type == ix::WebSocketMessageType::Message) {
-				this->add_received_messages(RadioReceivedMessage::MessageType::MESSAGE,
-				                            {{msg->str, RadioCurrentTimeMillis()}});
-			} else if (msg->type == ix::WebSocketMessageType::Open) {
-				this->activation_time_ = RadioCurrentTimeMillis();
-			} else if (msg->type == ix::WebSocketMessageType::Close) {
-				this->activation_time_ = 0;
-			} else if (msg->type == ix::WebSocketMessageType::Error) {
-				this->add_received_messages(RadioReceivedMessage::MessageType::ERROR,
-				                            {{msg->errorInfo.reason, RadioCurrentTimeMillis()}});
-			}
-		});
-
-		// FIXME: need a way to handle transmit messages in a seperate thread.
-
-		webSocket.start();
-	}
+	                           uint64_t creation_time, Radio &radio);
 
 	~RadioSubscription() {
 		webSocket.stop();
@@ -126,40 +102,9 @@ public:
 
 	// This could be called by another thread.
 	void add_received_messages(RadioReceivedMessage::MessageType type,
-	                           std::vector<std::pair<std::string, uint64_t>> messages,
-	                           uint64_t *message_ids = nullptr) {
-		std::vector<std::shared_ptr<RadioReceivedMessage>> items;
+	                           std::vector<std::pair<std::string, uint64_t>> messages, uint64_t *message_ids = nullptr);
 
-		for (size_t i = 0; i < messages.size(); i++) {
-			auto message_id = message_counter_.fetch_add(1, std::memory_order_relaxed);
-			if (message_ids) {
-				message_ids[i] = message_id;
-			}
-			auto entry =
-			    std::make_shared<RadioReceivedMessage>(*this, message_id, type, messages[i].first, messages[i].second);
-			items.push_back(entry);
-		}
-		get_queue_for_type(type).push(items);
-
-		radio_.NotifyHasMessages();
-	}
-
-	void add_transmit_messages(std::vector<RadioTransmitMessageParts> messages, uint64_t *message_ids) {
-		std::vector<std::shared_ptr<RadioTransmitMessage>> items;
-
-		auto current_time = RadioCurrentTimeMillis();
-		for (size_t i = 0; i < messages.size(); i++) {
-			auto message_id = message_counter_.fetch_add(1, std::memory_order_relaxed);
-			if (message_ids) {
-				message_ids[i] = message_id;
-			}
-			auto entry =
-			    std::make_shared<RadioTransmitMessage>(this->transmit_messages_, message_id, messages[i].message,
-			                                           current_time, messages[i].max_attempts, messages[i].expire_time);
-			items.emplace_back(std::move(entry));
-		}
-		transmit_messages_.push(items, current_time);
-	}
+	void add_transmit_messages(std::vector<RadioTransmitMessageParts> messages, uint64_t *message_ids);
 
 private:
 	// Store the ID of the subscription, it never changes.
